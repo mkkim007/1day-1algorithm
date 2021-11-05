bc37ba915b97b0687247321ed775d88df0a10bed
mkkim007 <alsrud991007@gmail.com>
2021-11-05 19:16:36 +0900

    Sort

--- Programmers/k번째수.py
+++ Programmers/k번째수.py
@@ -5,3 +5,9 @@ def solution(array, commands):
         ans = sorted(array[a[0]-1:a[1]])[a[2]-1]
         answer.append(ans)
     return answer
+
+
+# 다른 사람 코드,,
+def solution(array, commands):
+    # 존경,,,
+    return list(map(lambda x:sorted(array[x[0]-1:x[1]])[x[2]-1], commands))
