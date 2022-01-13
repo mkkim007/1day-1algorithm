7d3baba1e157052f4a7e6c016922fdcbe608ffca
mkkim007 <alsrud991007@gmail.com>
2022-01-13 23:39:52 +0900

    구현

--- 이것이_코딩_테스트다_한빛미디어/시각.py
+++ 이것이_코딩_테스트다_한빛미디어/시각.py
@@ -1,18 +1,8 @@
-def solution(n, action):
-    act = {"L":-1,"R":+1,"U":-1,"D":+1}
-    ans =[1,1]
-    temp =[1,1]
-    
-    for i in action :
-        if i=="L" or i=="R" :
-            temp[1] = temp[1] + act[i]
-        else :
-             temp[0] = temp[0] + act[i]
-
-
-        if temp[0]<=5 and temp[0]>=1 and temp[1]<=5 and temp[1]>=1 :
-            ans = temp.copy()  # 주의...
-        temp = ans.copy() # 주의 ...
-
-
-    return ans
+def solution(h):
+    count = 0
+    for i in range(h+1):
+        for j in range(60):
+            for k in range(60):
+                if '3' in str(i)+str(j)+str(k) :
+                    count+=1
+    return count
